fac05841009006c2ae8d880b806b9e964106f9c3
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-08 22:23:40 +0530

    Create getAWord.cpp

---
+++ getAWord.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  char ch[1];
+
+  //get a word
+  cin>>ch;
+  cout<<ch;
+  return 0;
+}
+
+*****IO***********************
+
+iam8bits
+iam8bits
+
+
+iam>8bits
+Segmentation fault (core dumped)
+
+iam8bits iam8bits
+iam8bits
+
+
+
+
+
+
