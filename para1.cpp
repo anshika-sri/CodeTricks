c61ae3b0eba3be0ab4777ae3176c3afb766b30ba
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-08 23:02:59 +0530

    Create para1.cpp

---
+++ para1.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char ch[10];
+
+
+cin.getline(ch, 9, '.');
+cout<<ch;
+return 0;
+}
+
+*****IO*********
+
+t,
+h,
+i,
+
+t,
+h,
+i,
+
+
+
+t,
+h,
+e.
+
+t,
+h,
+e
+
