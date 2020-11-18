d452d1525103c6f2c49700255ce9eae29753dda8
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-18 20:09:20 +0530

    Create sample1.cpp

---
+++ array/charArray/sample1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char s1[] = { "aamaa" };
+    char s2[] = { 'a' ,'b'};
+    
+    cout<<"s1: " <<s1[7]<<endl;
+    cout<<"s2: "<<s2[1];
+
+
+    return 0;
+}
+
+********IO***********
+s1: �
+s2: b
+
