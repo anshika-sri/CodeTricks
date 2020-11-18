d8aee03c622a7fa83625582565038b1e676edf27
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-18 19:45:04 +0530

    Create stringParagraph1.cpp

---
+++ array/charArray/stringParagraph1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string s1;
+
+    getline(cin, s1, '.');
+    cout<<s1;
+    return 0;
+}
+
+************************IO***************************
+ddfjkdfwldfwekfdjwe . iehwqekwqeqwe ihwqweeqwe
+ddfjkdfwldfwekfdjwe 
+
