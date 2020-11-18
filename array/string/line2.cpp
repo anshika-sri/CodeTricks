e0d2f083509c7faae73474a19a7bf47f5294257e
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-18 20:24:10 +0530

    Create line2.cpp

---
+++ array/string/line2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    string s1;
+
+    getline(cin, s1, '.');
+    cout<<s1[0];
+    return 0;
+}
+
+******IO*******
+
+sdf,df   kfwjefwef
+w;kef . jleqwe
+
+s
+
