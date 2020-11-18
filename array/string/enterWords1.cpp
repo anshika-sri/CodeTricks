f1bcb6addcb345ac9b53939c33044f9b5a360e2d
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-18 20:30:31 +0530

    Create enterWords1.cpp

---
+++ array/string/enterWords1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string s1[10];
+  
+  for(int i =0;i<10; i++) {
+      cin>>s1[i];
+  }
+
+  
+  cout<<s1[0] << s1[4];
+  return 0;
+}
+
+********IO*********
+
+anshika1
+anshika2
+anshika3 
+anshika4
+.
+klrer.
+w'rl
+wjrwelr
+e
+e
+
+anshika1.
+
+
+a n s h i k a s r i v a s t a v a
+
+ai
+
+
+
