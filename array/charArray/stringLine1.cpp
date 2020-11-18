557360d9cdef86ffd01ef484c5be3a18392a1b24
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-18 19:44:09 +0530

    Create stringLine1.cpp

---
+++ array/charArray/stringLine1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string s1;
+
+  getline(cin, s1);
+  cout<<s1;
+  return 0;
+}
+
+**************IO******************
+hsdskdshdsdf bjdshsgkss kihwkddede
+hsdskdshdsdf bjdshsgkss kihwkddede
+
