11c6e8898e0ac92b43778c551f5c86a16d483d36
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-18 20:57:50 +0530

    Create enterKeyAndMemory1.cpp

---
+++ array/string/enterKeyAndMemory1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string s1[10];
+  
+  for(int i =0;i<10; i++) {
+      cin>>s1[i];
+  }
+ for(int i =0;i<10; i++) {
+           cout<<s1[i]<<" "<<s1<<" " << s1+i<<endl;
+}
+  
+  cout<<s1[0] << s1[4];
+  return 0;
+}
+
+
+
+****************************************IO**************************************************
+
+abc def j k l m n o p q s q q qe t u i k n 
+
+abc 0x7fff2f936b90 0x7fff2f936b90 //140733991578512
+def 0x7fff2f936b90 0x7fff2f936b98 //140733991578520
+j 0x7fff2f936b90 0x7fff2f936ba0 //140733991578528
+k 0x7fff2f936b90 0x7fff2f936ba8 //140733991578536
+l 0x7fff2f936b90 0x7fff2f936bb0
+m 0x7fff2f936b90 0x7fff2f936bb8
+n 0x7fff2f936b90 0x7fff2f936bc0
+o 0x7fff2f936b90 0x7fff2f936bc8
+p 0x7fff2f936b90 0x7fff2f936bd0 //140733991578576
+q 0x7fff2f936b90 0x7fff2f936bd8 //140733991578584
+abcl
+
+
+
+anshika1112 1 2 3 4 5 6 78 9  1 2 3 4 6 
+
+anshika1112 0x7ffe6ecf12f0 0x7ffe6ecf12f0
+1 0x7ffe6ecf12f0 0x7ffe6ecf12f8
+2 0x7ffe6ecf12f0 0x7ffe6ecf1300
+3 0x7ffe6ecf12f0 0x7ffe6ecf1308
+4 0x7ffe6ecf12f0 0x7ffe6ecf1310
+5 0x7ffe6ecf12f0 0x7ffe6ecf1318
+6 0x7ffe6ecf12f0 0x7ffe6ecf1320
+78 0x7ffe6ecf12f0 0x7ffe6ecf1328
+9 0x7ffe6ecf12f0 0x7ffe6ecf1330
+1 0x7ffe6ecf12f0 0x7ffe6ecf1338
+anshika11124
+
+
+1234567890 12   1234567890  1234567890 0 0 1 2 3    4 5 6 7 8 9 0 1 2 3 4
+123456789012345678901234567890 0x7ffe042aa840 0x7ffe042aa840
+0 0x7ffe042aa840 0x7ffe042aa848
+0 0x7ffe042aa840 0x7ffe042aa850
+1 0x7ffe042aa840 0x7ffe042aa858
+2 0x7ffe042aa840 0x7ffe042aa860
+3 0x7ffe042aa840 0x7ffe042aa868
+4 0x7ffe042aa840 0x7ffe042aa870
+5 0x7ffe042aa840 0x7ffe042aa878
+6 0x7ffe042aa840 0x7ffe042aa880
+7 0x7ffe042aa840 0x7ffe042aa888
+1234567890123456789012345678902
+
+
+ans.hik-a is ty p i n g 
+a n d t y p i ng
+
+ans.hik-a 0x7ffe0312b600 0x7ffe0312b600
+is 0x7ffe0312b600 0x7ffe0312b608
+ty 0x7ffe0312b600 0x7ffe0312b610
+p 0x7ffe0312b600 0x7ffe0312b618
+i 0x7ffe0312b600 0x7ffe0312b620
+n 0x7ffe0312b600 0x7ffe0312b628
+g 0x7ffe0312b600 0x7ffe0312b630
+a 0x7ffe0312b600 0x7ffe0312b638
+n 0x7ffe0312b600 0x7ffe0312b640
+d 0x7ffe0312b600 0x7ffe0312b648
+ans.hik-ai
+
+a n
+sh
+
+i k 
+a
+
+
+s
+p
+a
+c (here enter pressed)
+a 0x7fff06a6bc60 0x7fff06a6bc60
+n 0x7fff06a6bc60 0x7fff06a6bc68
+sh 0x7fff06a6bc60 0x7fff06a6bc70
+i 0x7fff06a6bc60 0x7fff06a6bc78
+k 0x7fff06a6bc60 0x7fff06a6bc80
+a 0x7fff06a6bc60 0x7fff06a6bc88
+s 0x7fff06a6bc60 0x7fff06a6bc90
+p 0x7fff06a6bc60 0x7fff06a6bc98
+a 0x7fff06a6bc60 0x7fff06a6bca0
+c 0x7fff06a6bc60 0x7fff06a6bca8
+ak
+
+
+a n s h i    k a i s t y p i n g an d   t(here enter pressed)
+a 0x7ffe89ca1510 0x7ffe89ca1510
+n 0x7ffe89ca1510 0x7ffe89ca1518
+s 0x7ffe89ca1510 0x7ffe89ca1520
+h 0x7ffe89ca1510 0x7ffe89ca1528
+i 0x7ffe89ca1510 0x7ffe89ca1530
+k 0x7ffe89ca1510 0x7ffe89ca1538
+a 0x7ffe89ca1510 0x7ffe89ca1540
+i 0x7ffe89ca1510 0x7ffe89ca1548
+s 0x7ffe89ca1510 0x7ffe89ca1550
+t 0x7ffe89ca1510 0x7ffe89ca1558
+ai
+
+
+
+
+
