6dd37c0ff32b43c7156df705e7f63edf4bf16e76
anshika-sri <35023358+anshika-sri@users.noreply.github.com>
2020-11-29 20:19:53 +0530

    Create charArr1.cpp

---
+++ Errors/stackErr/charArr1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char ch[7];
+
+  //get a word
+  cin>>ch;
+  cout<<ch;
+  return 0;
+}
+
+
+*****IO********
+
+iam8bits
+*** stack smashing detected ***: ./a.out terminated
+iam8bitsAborted (core dumped)
+
+
+iam
+iam
